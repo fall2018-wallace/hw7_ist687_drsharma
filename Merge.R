edff089a712107b10f232365b6f759ae63707c56
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:26:45 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -4,6 +4,6 @@ arrests <- cbind(rownames(arrests),arrests)
 colnames(arrests) <- c("stateName")
 
 #Merge the two dataframes
-merged_data <- merge(census, arrests)
+merged_data <- merge(merge, arrests)
 View(merged_data)
 colnames(merged_data) <- c("stateName","population","popOver18","percentOver18","Murder","Assault","UrbanPop","Rape")
