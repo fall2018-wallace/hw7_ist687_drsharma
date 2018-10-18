523577551a8ee4c195a6e84c70a623b880075687
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:30:27 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,4 +1,5 @@
 
+#Reading the census data and then removing the first 4 columns and row numbers 1, 10 and 53 and renaming the columns
 census <- census[,-1:-4]
 census <- census[-c(1,53,10),]
 colnames(census) <- c("stateName","population","popOver18","percentOver18")
