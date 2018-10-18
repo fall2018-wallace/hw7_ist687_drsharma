e5276bf99fd61d2a62bdad013a6f97459356b250
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:38:21 -0400

    Saving Population.R

--- Population.R
+++ Population.R
@@ -6,6 +6,7 @@ library(ggmap)
 #Using map_data function
 US=map_data("state")
 
+#Calculting the population of every state and respresenting it by a circle at the center of the state and the larger the circle means the larger the population of that particular state
 pop_large=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 pop_large=pop_large + expand_limits(x=US$long, y=US$lat)
 
