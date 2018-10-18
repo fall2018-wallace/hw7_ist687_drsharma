6c830bfaff82a6e0e020665a37070091ed83042b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:36:13 -0400

    Saving state_center.R

--- state_center.R
+++ state_center.R
@@ -3,14 +3,18 @@
 library(ggplot2)
 library(ggmap)
 
+#Getting the areas and nams of the states in US
 state.area
 state.name
 
+#Merging the state areas to the data frame
 merged_data$area= state.area
 
 statecenter= data.frame(state.center)
+#Merging the locations of the states to the data frame
 merged_data$latitude=statecenter$y
 merged_data$longitude=statecenter$x
 
+#Converting the Upper case alphabets to lower cas in the stateName column of the data frame "merged_data"
 merged_data$stateName=tolower(merged_data$stateName)
 US=map_data("state")
