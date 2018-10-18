db9ca0ac339ad6f9efc6e95608a621726956b9d9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:43:04 -0400

    Saving Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -6,9 +6,9 @@ library(ggmap)
 US=map_data("state")
 
 #Calculating the area of the US States and displaying it through the map with the help of a particular color code
-Area=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="red")
+Area=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
 Area=Area + expand_limits(x=US$long, y=US$lat) + ggtitle("Area of US states") + coord_map()
 
 #Calculating the murder rate in the US and displaying it through the map with the help of a particular color code
-murder=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="red")
+murder=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 murder=murder + expand_limits(x=US$long, y=US$lat) + ggtitle("Murder rate in US states") + coord_map()
