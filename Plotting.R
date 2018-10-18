248cf307c1cbde6dabd8f1a3ee109048ea17a6b0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:40:32 -0400

    Saving Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -1,8 +1,11 @@
 
+#Loading the libraries in order to visualize the data
 library(ggplot2)
 library(ggmap)
 
 US=map_data("state")
+
+#Calculating the area of the US States and displaying it through the map
 Area=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
 Area=Area + expand_limits(x=US$long, y=US$lat) + ggtitle("Area of US states") + coord_map()
 
