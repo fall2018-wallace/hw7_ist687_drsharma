721d9496da4de8d972b196423533bd3d9f3f1c85
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:31:46 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -6,4 +6,5 @@ colnames(arrests) <- c("stateName")
 
 #Merge the two dataframes
 merged_data <- merge(merge, arrests)
+#Renaming all the columns of the data frame
 colnames(merged_data) <- c("stateName","population","popOver18","percentOver18","Murder","Assault","UrbanPop","Rape")
