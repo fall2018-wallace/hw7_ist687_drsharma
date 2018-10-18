6350169f1ae0dcdb644776c36fd46f1cd4a5a584
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:42:29 -0400

    Saving Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -1,7 +1,7 @@
 
 library(ggplot2)
 library(ggmap)
-
+US=map_data)("state")
 Area=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
 Area=Area + expand_limits(x=US$long, y=US$lat) + ggtitle("Area of US states") + coord_map()
 
