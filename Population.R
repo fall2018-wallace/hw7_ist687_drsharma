edbc422b628776217eb374b9c0ed1ce5eeefaeaa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:05:09 -0400

    Saving Population.R

--- Population.R
+++ Population.R
@@ -4,8 +4,8 @@ library(ggmap)
 
 US=map_data("state")
 
-pop=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
-pop=pop + expand_limits(x=US$long, y=US$lat)
+pop_large=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
+pop_large=pop_large + expand_limits(x=US$long, y=US$lat)
 
-pop= pop + geom_point(aes(x=merged_data$longitude, y=merged_data$latitude, size=merged_data$population)) + coord_map()
-pop=pop + ggtitle("Population")
+pop_large= pop_large + geom_point(aes(x=merged_data$longitude, y=merged_data$latitude, size=merged_data$population)) + coord_map()
+pop_large=pop_large + ggtitle("Population")
