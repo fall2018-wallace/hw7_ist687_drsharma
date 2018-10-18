e765b9fd494a2ae8d9ce3dea918df7102ddc737a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 01:33:56 -0400

    Saving Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -1,1 +1,16 @@
 
+library(ggplot2)
+library(ggmap)
+
+state.area
+state.name
+
+merged_data$area= state.area
+
+statecenter= data.frame(state.center)
+View(statecenter)
+merged_data$latitude=statecenter$y
+merged_data$longitude=statecenter$x
+
+merged_data$stateName=tolower(merged_data$stateName)
+US=map_data("state")
