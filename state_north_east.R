38ac8873e153f59077f0ed89cf013e63a62feb96
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 02:08:45 -0400

    Saving state_north_east.R

--- state_north_east.R
+++ state_north_east.R
@@ -1,1 +1,10 @@
 
+library(ggplot2)
+install.packages("ggmap")
+library(ggmap)
+
+northeast_murder=  murder+ xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_murder
+
+northeast_pop= pop + xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_pop
